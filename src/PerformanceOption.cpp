777837523a5fbcf3fa163d0a841c360d5c829537
Lucas Macias <maciasl@ensipc129.ensimag.fr>
2022-09-13 11:36:30 +0200

    PerformanceOption

--- src/PerformanceOption.cpp
+++ src/PerformanceOption.cpp
@@ -5,9 +5,6 @@
 class PerformanceOption :: Option
 {
   public:
-    double T_;        /// maturité
-    int nbTimeSteps_; /// nombre de pas de temps de discrétisation
-    int size_;        /// dimension du modèle, redondant avec BlackScholesModel::size_
     /**
      * Calcule la valeur du payoff sur la trajectoire
      *
@@ -16,5 +13,32 @@ class PerformanceOption :: Option
      * par la fonction asset.
      * @return phi(trajectoire)
      */
-    double payoff(const PnlMat* path) = 0;
+    PerformanceOption::PerformanceOption(double T, int nbTimeSteps, int size, PnlVect coefficients){
+        this->T_ = T;
+        this->nbTimeSteps_ = nbTimeSteps;
+        this->coefficients_ = coefficients;
+        this->size_ = size;
+    }
+
+    double PerformanceOption::payoff(const PnlMat* path){
+
+        double payoff = 1;
+        for(int i = 0; i < nbTimeSteps; i++){
+            double numerator = 0;
+            double denominator = 0;
+            for(int d = 0; d < size; d++){
+                double lambda = coefficients->array[d];
+                numerator += lambda * path->array[d][i+1];
+                denominator += lambda * path->array[d][i];
+            }
+            if (numerator > denominator){
+                payoff += numerator/denominator - 1;
+            }
+        }
+
+        return payoff;
+        
+
+    };
+
 };
--- src/PerformanceOption.hpp
+++ src/PerformanceOption.hpp
@@ -7,9 +7,7 @@
 class PerformanceOption :: Option
 {
   public:
-    double T_;        /// maturité
-    int nbTimeSteps_; /// nombre de pas de temps de discrétisation
-    int size_;        /// dimension du modèle, redondant avec BlackScholesModel::size_
+    PnlVect* coefficients;
     /**
      * Calcule la valeur du payoff sur la trajectoire
      *
@@ -18,5 +16,6 @@ class PerformanceOption :: Option
      * par la fonction asset.
      * @return phi(trajectoire)
      */
+    PerformanceOption(double T, int nbTimeSteps, int size, PnlVect coefficients);
     double payoff(const PnlMat* path) = 0;
 };
