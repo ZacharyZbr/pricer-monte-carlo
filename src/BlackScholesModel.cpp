5ea54bab978c06e03b1e098cde614979c23179e1
Gaspard Moizo <gaspard.moizo@grenoble-inp.org>
2022-09-14 11:46:18 +0200

    adjustment in blackscholes model : added multidimension formula

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -22,18 +22,31 @@ BlackScholesModel::BlackScholesModel(int size, double r, double rho, PnlVect *si
 
 void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *rng)
 {
-    printf("Début de la boucle \n");
-    printf("On itère sur %d avec %d de pas \n", size_, nbTimeSteps);
+
+    PnlMat *correlationMat = pnl_mat_create_from_scalar(size_, size_, rho_);
+    pnl_mat_set_diag(correlationMat, 1, 0);
+    pnl_mat_chol(correlationMat);
+
     for (int underlyingAsset = 0; underlyingAsset < size_; underlyingAsset++)
     {
-        printf("asset %d : \n", underlyingAsset);
-        for (int k = 0; k < nbTimeSteps; k++)
+
+        double volatility = pnl_vect_get(sigma_, underlyingAsset);
+
+        pnl_mat_set(path, underlyingAsset, 0, pnl_vect_get(spot_, 0));
+
+        PnlVect *row_chol;
+        pnl_mat_get_row(row_chol, correlationMat, underlyingAsset);
+
+        for (int k = 1; k < nbTimeSteps; k++)
         {
-            double brownian_t = pnl_rng_normal(rng) * pow((k * T / nbTimeSteps), 2);
-            double s_0 = spot_->array[underlyingAsset];
-            double volatility_0 = sigma_->array[underlyingAsset];
-            printf("We put %f value in place [%d, %d] \n", s_0 * exp((r_ - pow(volatility_0, 2) / 2) + volatility_0 * brownian_t), underlyingAsset, k);
-            pnl_mat_set(path, underlyingAsset, k, s_0 * exp((r_ - pow(volatility_0, 2) / 2) + volatility_0 * brownian_t));
+            PnlVect *G = pnl_vect_new();
+            pnl_vect_rng_normal(G, nbTimeSteps, pnl_rng_create(PNL_RNG_MERSENNE));
+
+            double scalar_product = pnl_vect_scalar_prod(row_chol, G);
+
+            double new_price = pnl_mat_get(path, underlyingAsset, k - 1) * exp((r_ - pow(volatility, 2) / 2) * ((T / nbTimeSteps)) + (volatility * sqrt((T / nbTimeSteps)) * scalar_product));
+
+            pnl_mat_set(path, underlyingAsset, k, new_price);
         }
     }
 }
